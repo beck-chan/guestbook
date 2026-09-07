f1dd5f04ce51f7adea2393c4515c0594a7b2f699
Beck Chan <rebecca.ht.chan@gmail.com>
2026-09-06 17:09:13 -0700

    copy code button

--- app/docs/_components/DocsCodeBlock.tsx
+++ app/docs/_components/DocsCodeBlock.tsx
@@ -2,39 +2,6 @@
 
 import { useEffect, useRef, useState } from "react";
 
-function CopyIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-      aria-hidden="true"
-    >
-      <path
-        fillRule="evenodd"
-        d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
-      />
-    </svg>
-  );
-}
-
-function CopiedIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      viewBox="0 0 16 16"
-      aria-hidden="true"
-    >
-      <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
-    </svg>
-  );
-}
-
 export function DocsCodeBlock({
   children,
   className,
@@ -76,10 +43,8 @@ export function DocsCodeBlock({
         type="button"
         className={`docs-code-copy${copied ? " is-copied" : ""}`}
         onClick={copy}
-        aria-label={copied ? "Copied to clipboard" : "Copy code to clipboard"}
-      >
-        {copied ? <CopiedIcon /> : <CopyIcon />}
-      </button>
+        aria-label={copied ? "Copied" : "Copy"}
+      />
       <pre
         ref={preRef}
         {...props}
