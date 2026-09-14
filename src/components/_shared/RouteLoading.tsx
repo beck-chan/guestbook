be9c21858ce6c61d8f058cc48bea6b7798d49a53
Beck Chan <rebecca.ht.chan@gmail.com>
2026-09-14 08:01:00 -0700

    Save

--- src/components/_shared/RouteLoading.tsx
+++ src/components/_shared/RouteLoading.tsx
@@ -1,7 +1,7 @@
 export function RouteLoading() {
   return (
     <p className="route-loading" role="status">
-      <span>And now the matchless deed 's achieved,</span>
+      <span>{"And now the matchless deed 's achieved,"}</span>
       <span>Determined, dared, and done!</span>
     </p>
   );
