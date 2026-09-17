47b0507c17d9c79db711448f366c16b527c21068
Beck <rebecca.ht.chan@gmail.com>
2026-09-16 19:38:52 -0700

    hotfix: PostHog scoping (#2)
    * posthog fix
    
    * save

--- next.config.ts
+++ next.config.ts
@@ -15,8 +15,6 @@ const nextConfig: NextConfig = {
     FLAG_DOCS: process.env.FLAG_DOCS,
     FLAG_DOCSONLY: process.env.FLAG_DOCSONLY,
     FLAG_COUNTER: process.env.FLAG_COUNTER,
-    FLAG_COUNTER_URL: process.env.FLAG_COUNTER_URL,
-    FLAG_COUNTER_DATE: process.env.FLAG_COUNTER_DATE,
     FLAG_PUBLIC: process.env.FLAG_PUBLIC,
     FLAG_APITEST: process.env.FLAG_APITEST,
   },
